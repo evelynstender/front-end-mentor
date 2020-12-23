f2ad0e838f447fd2f9670425df9a4d7a630a03d7
Evelyn Stender <evelyn@beapplied.com>
2020-12-23 14:02:24 +0000

    a

--- profile-card-component-main/src/App.js
+++ profile-card-component-main/src/App.js
@@ -6,14 +6,17 @@ function App() {
           {/* <header className="App-header"> */}
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
           {/* </header> */}
-          <div className="cardWrapper">Victor Crest 26 London 80K Followers 803K Likes 1.4K Photos</div>
+          <div className="cardWrapper">
+              Victor Crest 26 London 80K Followers 803K Likes 1.4K Photos
+          </div>
           <div className="attribution">
               Challenge by
               <a
                   href="https://www.frontendmentor.io?ref=challenge"
                   target="_blank"
+                  rel="noreferrer"
               >
-                   Frontend Mentor
+                  Frontend Mentor
               </a>
               . Coded by <a href="https://evelynstender.com">Evelyn Stender</a>.
           </div>
